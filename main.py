25fe41cea7ed0f20a9b109a332d2891028438926
obna <obn.adeleke@gmail.com>
2022-09-20 09:37:22 -0700

    Add qrs to directory

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 import wifi_qrcode_generator
 import qrcode
 import cv2
+import os
+import shutil
 
-qrtype = int(input("1) Website\t2) Wifi\t3) Scan\nEnter 1, 2, or 3: "))
+
+def move_it(toBeMoved):
+    bake = shutil.move(toBeMoved, finalDestination)
+    print('A new QR code has generated on path ' + bake[1:])
 
 
 def qrd_website():
-    url = input('Input URL')
+    url = input('Input URL: ')
     filename = "qrd_site.png"
     img = qrcode.make(url)
     img.save(filename)
+    moveThis = foundHere + '/' + filename
+    move_it(moveThis)
 
+
+def qrd_wifi():
+    name = input('Enter WPA wifi name')
+    value = input('Enter password')
+    wifi_qrcode_generator.wifi_qrcode(
+        name, False, 'WPA', value)
+
+
+def qrd_scan():
+    pass
+
+
+foundHere = os.path.dirname(os.path.abspath(__file__))
+finalDestination = '/'+foundHere+"/qr-codes"
+
+qrtype = int(input("Enter 1 for Website, 2 for Wifi, 3 to Scan, 4 to stop: "))
 
 if qrtype == 1:
     qrd_website()
+if qrtype == 2:
+    pass
+if qrtype == 3:
+    pass
